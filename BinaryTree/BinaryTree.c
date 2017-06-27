2d06e67a180804943c44ae2c2575e99eb349c394
Bin Zhu <zhubin@gmail.com>
2017-06-27 18:24:02 -0500

    Add BinaryTree code with 3 different Traversal; Create Iterator class for 3 different tree traversals; Add test code to verify it.

---
+++ BinaryTree/BinaryTree.c
@@ -0,0 +1,361 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stack>
+
+struct node {
+   int data;
+    
+   struct node *leftChild;
+   struct node *rightChild;
+};
+
+struct node *root = NULL;
+
+void insert(int data) {
+   struct node *tempNode = (struct node*) malloc(sizeof(struct node));
+   struct node *current;
+   struct node *parent;
+
+   tempNode->data = data;
+   tempNode->leftChild = NULL;
+   tempNode->rightChild = NULL;
+
+   //if tree is empty
+   if(root == NULL) {
+      root = tempNode;
+   } else {
+      current = root;
+      parent = NULL;
+
+      while(1) {
+         parent = current;
+         
+         //go to left of the tree
+         if(data < parent->data) {
+            current = current->leftChild;                
+            
+            //insert to the left
+            if(current == NULL) {
+               parent->leftChild = tempNode;
+               return;
+            }
+         }//go to right of the tree
+         else {
+            current = current->rightChild;
+
+            //insert to the right
+            if(current == NULL) {
+               parent->rightChild = tempNode;
+               return;
+            }
+         }
+      }            
+   }
+}
+
+struct node* search(int data) {
+   struct node *current = root;
+   printf("Visiting elements: ");
+
+   while(current->data != data) {
+      if(current != NULL)
+         printf("%d ",current->data);
+
+      //go to left tree
+      if(current->data > data) {
+         current = current->leftChild;
+      }
+      //else go to right tree
+      else {                
+         current = current->rightChild;
+      }
+
+      //not found
+      if(current == NULL) {
+         return NULL;
+      }
+   }
+   
+   return current;
+}
+
+void pre_order_traversal(struct node* root) {
+   if(root != NULL) {
+      printf("%d ",root->data);
+      pre_order_traversal(root->leftChild);
+      pre_order_traversal(root->rightChild);
+   }
+}
+
+void inorder_traversal(struct node* root) {
+   if(root != NULL) {
+      inorder_traversal(root->leftChild);
+      printf("%d ",root->data);          
+      inorder_traversal(root->rightChild);
+   }
+}
+
+void post_order_traversal(struct node* root) {
+   if(root != NULL) {
+      post_order_traversal(root->leftChild);
+      post_order_traversal(root->rightChild);
+      printf("%d ", root->data);
+   }
+}
+
+/* implement an Iterator class to sort */
+template <class T>
+class Iterator
+{
+    public:    
+    Iterator(){};
+    ~Iterator(){};
+    
+     public:
+     bool hasNext(){return false;};
+     T  next(){return NULL;};
+};
+
+class InOrderNodeIterator: public Iterator <node*>
+{
+      public:
+       InOrderNodeIterator(node* root);
+       ~InOrderNodeIterator(){};
+      public:
+        bool hasNext();
+        node* next();
+        int  maxStackDepth(){ return stackDepth;};
+      protected:
+        void  initStack(node*);
+      private:
+        std::stack <node*> mStack;
+        int stackDepth;
+};
+
+InOrderNodeIterator::InOrderNodeIterator(node* root)
+{
+   stackDepth = 0;
+   initStack(root);
+}
+
+bool InOrderNodeIterator::hasNext()
+{
+     return !mStack.empty();
+}
+
+node* InOrderNodeIterator::next()
+{
+    /* check the right child and go to very left of the node */
+    node* pNode = mStack.top();
+    mStack.pop();
+
+    if(pNode->rightChild){
+        initStack(pNode->rightChild);
+    }
+    
+    return pNode;
+}  
+
+/* protected */
+void InOrderNodeIterator::initStack(node* root)
+{
+    mStack.push(root);
+    
+    while(root->leftChild){
+        mStack.push(root->leftChild);
+        root = root->leftChild;
+    }
+    
+    if(mStack.size() > stackDepth)
+        stackDepth = mStack.size();
+}
+
+/* pre-order Iterator */
+class PreOrderNodeIterator: public Iterator <node*>
+{
+      public:
+       PreOrderNodeIterator(node* pRoot);
+       ~PreOrderNodeIterator(){};
+      public:
+        bool hasNext();
+        node* next();
+        int  maxStackDepth(){ return stackDepth;};
+      protected:
+        void  initStack(node* pNode);
+      private:
+        std::stack <node*> mStack;
+        int stackDepth;
+};
+
+PreOrderNodeIterator::PreOrderNodeIterator(node* root)
+{
+    stackDepth = 2;
+    mStack.push(root);
+}
+
+bool PreOrderNodeIterator::hasNext()
+{
+     return !mStack.empty();
+}
+
+node* PreOrderNodeIterator::next()
+{
+   /* check the right child and go to very left of the node */
+   node* pTmp = mStack.top();
+    mStack.pop();
+    
+    if(pTmp!= NULL){       
+         if(pTmp->rightChild){
+            mStack.push(pTmp->rightChild);
+         }
+         if(pTmp->leftChild){
+             mStack.push(pTmp->leftChild);
+         }
+    }
+    
+    return pTmp;
+
+}  
+
+/* protected */
+void PreOrderNodeIterator::initStack(node* root)
+{
+    /* don't need it */
+}
+
+/* post-order Iterator */
+class PostOrderNodeIterator: public Iterator <node*>
+{
+      public:
+       PostOrderNodeIterator(node* pRoot);
+       ~PostOrderNodeIterator(){};
+      public:
+        bool hasNext();
+        node* next();
+        int  maxStackDepth(){ return stackDepth;};
+      protected:
+        void  initStack(node* pNode);
+      private:
+        std::stack <node*> mStack;
+        int stackDepth;
+};
+
+PostOrderNodeIterator::PostOrderNodeIterator(node* root)
+{
+    stackDepth = 0;
+    if(root){
+        mStack.push(root);
+        initStack(root);
+    }
+}
+
+bool PostOrderNodeIterator::hasNext()
+{
+     return !mStack.empty();
+}
+
+node* PostOrderNodeIterator::next()
+{
+    /* check the right child and go to very left of the node */
+    node* pNode = mStack.top();
+    mStack.pop();
+    
+    if(mStack.empty())
+        return pNode;
+    
+    node* pCur = mStack.top();
+    
+    if(!pCur || pCur->leftChild == pNode || pCur->rightChild == pNode)
+        return pNode;
+    
+    initStack(pCur);
+    
+    return pNode;
+}  
+
+/* protected */
+void PostOrderNodeIterator::initStack(node* root)
+{
+    node* pNode = NULL;
+    
+    while(root){
+        pNode = NULL;
+        if(root->rightChild){
+            mStack.push(root->rightChild);
+            pNode = root->rightChild;
+        }    
+        if(root->leftChild){
+            mStack.push(root->leftChild);
+            pNode = root->leftChild;
+        }
+        
+        root = pNode;
+    }
+
+    if(mStack.size() > stackDepth)
+        stackDepth = mStack.size();
+}
+
+#define MAX 20
+int main() {
+   int i;
+   int array[MAX] = { 27, 14, 35, 15, 36,34,99, 25, 12 ,45,89, 54,38,24, 66, 96, 37, 11, 55,32};
+
+   for(i = 0; i < MAX; i++)
+      insert(array[i]);
+
+   i = 31;
+   struct node * temp = search(i);
+
+   if(temp != NULL) {
+      printf("[%d] Element found.", temp->data);
+      printf("\n");
+   }else {
+      printf("[ x ] Element not found (%d).\n", i);
+   }
+
+   i = 15;
+   temp = search(i);
+
+   if(temp != NULL) {
+      printf("[%d] Element found.", temp->data);
+      printf("\n");
+   }else {
+      printf("[ x ] Element not found (%d).\n", i);
+   }            
+
+   printf("\nPreorder traversal: ");
+   pre_order_traversal(root);
+
+   printf("\nInorder traversal: ");
+   inorder_traversal(root);
+
+   printf("\nPost order traversal: ");
+   post_order_traversal(root);       
+   
+   printf("\nPreorder traversal w Iterator:\n[ ");
+   PreOrderNodeIterator preIter(root);
+   while(preIter.hasNext()){
+       printf("%d ", preIter.next()->data);
+   }
+   printf("]\n");
+   printf("MaxStackDepth: %d\n", preIter.maxStackDepth());
+
+   printf("\nInorder traversal w Iterator:\n[ ");
+   InOrderNodeIterator inIter(root);
+   while(inIter.hasNext()){
+       printf("%d ", inIter.next()->data);
+   }
+   printf("]\n");
+   printf("MaxStackDepth: %d\n", inIter.maxStackDepth());
+    
+   printf("\nPostorder traversal w Iterator:\n[ ");
+   PostOrderNodeIterator postIter(root);
+   while(postIter.hasNext()){
+       printf("%d ", postIter.next()->data);
+   }
+   printf("]\n");
+   printf("MaxStackDepth: %d\n", postIter.maxStackDepth());
+    
+   return 0;
+}
